01569281730909a6aa0f8e377f44811aa74f8639
m.darkhovskiy <m.darkhovskiy@qiwi.com>
2020-02-04 11:21:49 +0300

    Statistics banner (different heights, fix font and colour)

--- js/stat.js
+++ js/stat.js
@@ -1,34 +1,40 @@
 window.renderStatistics = function (ctx, names, times) {
-  var width = 420;
-  var height = 270;
-  var x = 100;
-  var y = 10;
+  var WIDTH = 420;
+  var HEIGHT = 270;
+  var X = 100;
+  var Y = 10;
 
-  var congrats = 'Ура вы победили! \nСписок результатов: ';
+  var CONGRATS = 'Ура вы победили! \nСписок результатов: ';
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(x + 10, y + 10, width, height);
+  ctx.fillRect(X + 10, Y + 10, WIDTH, HEIGHT);
   ctx.fillStyle = 'white';
-  ctx.fillRect(x, y, width, height);
-  ctx.font = '16px pt monospace';
+  ctx.fillRect(X, Y, WIDTH, HEIGHT);
+  ctx.font = '16px PT mono';
   ctx.fillStyle = 'black';
-  ctx.fillText(congrats.split('\n')[0], x + 30, y + 30);
-  ctx.fillText(congrats.split('\n')[1], x + 30, y + 40);
+  ctx.fillText(CONGRATS.split('\n')[0], X + 30, Y + 30);
+  ctx.fillText(CONGRATS.split('\n')[1], X + 30, Y + 50);
 
-  var intervalHist = 50;
-  var hTime = 150;
-  var shiftX = 120;
-  var shiftY = 70;
+  var INTERVALHIST = 50;
+  var COL_WIDTH = 40;
+  var HTIME = 150;
+  var SHIFTX = 60;
+  var SHIFTY = 80;
+  var MAX_TIME = times[0];
+  // Array of histogram heights
+  for (var i = 1; i <= times.length - 1; i++) {
+    if ( times[i] > MAX_TIME ) {
+        MAX_TIME = times[i];
+    }
+  }
   for (var i = 0; i <= names.length - 1; i++) {
     if (names[i] == 'Вы') {
-      hTime = 150;
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      var saturation = Math.random();
-      ctx.fillStyle = 'hsl(246, 100%, 44%, ' + saturation + ')';
+      ctx.fillStyle = 'hsla(246, 100%, ' + Math.random() * 100 + '%, 1)';
     }
-    ctx.fillRect(x + i * intervalHist + shiftX, y + shiftY + 10, 40, hTime);
-    //ctx.fillStyle = 'black';
-    ctx.fillText(names[i], x + i * intervalHist + shiftX, y + shiftY);
-    ctx.fillText(Math.round(times[i]), x + i * intervalHist + shiftX, y + shiftY + 180);
+    ctx.fillRect(X + i * (COL_WIDTH + INTERVALHIST) + SHIFTX, Y + SHIFTY + 10 + HTIME - HTIME * times[i] / MAX_TIME, COL_WIDTH, HTIME * times[i] / MAX_TIME);
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], X + i * (COL_WIDTH + INTERVALHIST) + SHIFTX, Y + SHIFTY);
+    ctx.fillText(Math.round(times[i]), X + i * (COL_WIDTH + INTERVALHIST) + SHIFTX, Y + SHIFTY + 180);
   }
 };
